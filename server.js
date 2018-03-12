b60f2f2bb1c0c695d773ada5e1e650892c2ee8fb
Glitch (filemetadata1) <none>
2018-03-12 06:35:57 +0000

    🎃🤑 Checkpoint server.js:471758/72

--- server.js
+++ server.js
@@ -9,12 +9,13 @@ app.use(bodyParse.json());
 app.use(cors());
 
 app.get('/', function(req,res,next){
-res.sendFile(__dirname + "/index.html");
+res.sendFile(__dirname + "/views/index.html");
 });
 
+app.use(express.static(__dirname + '/public'));
 
 app.post('/upload',upload.single('file'), function(req,res,next){
-
+return res.json(req.file);
 });
 
 
