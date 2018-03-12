b3fb6362c48d3bc416c2c4e3f202162b1a1e07ed
Glitch (filemetadata1) <none>
2018-03-12 06:25:54 +0000

    ☄️📒 Checkpoint server.js:471758/217 views/index.html:471758/201

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ var express = require('express');
 var app = module.exports = express();
 var bodyParse = require('body-parser');
 var cors = require('cors');
-var mukter = require('multer');
+var multer = require('multer');
+var upload = multer({dest:'uploads/'});
 
 app.use(bodyParse.json());
 app.use(cors());
 
+app.get('/', function(req,res,next){
+res.sendFile(__dirname + "/index.html");
+});
 
+
+app.post('/upload',upload.single('file'), function(req,res,next){
+
+});
 
 
 
