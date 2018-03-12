f911ba1f837ba145e15a751b58cd1dfdbe31a6ed
Glitch (filemetadata1) <none>
2018-03-12 06:15:51 +0000

    📇🍓 Checkpoint README.md:471758/13 package.json:471758/30 server.js:471758/238 views/index.html:471758/79

--- server.js
+++ server.js
@@ -1,39 +1,19 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
-var app = express();
+var app = module.exports = express();
+var bodyParse = require('body-parser');
+var cors = require('cors');
+var mukter = require('multer');
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+app.use(bodyParse.json());
+app.use(cors());
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
 
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
-// listen for requests :)
+
+
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
